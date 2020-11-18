325a9a1c0f2c2e58574382d0400642de1fae5a6f
Gregory Eales <gregoeales@aol.com>
2020-11-18 12:33:55 -0800

    update

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -7,17 +7,17 @@ from time import time
 # Block Format
 
 block = {
-    'index': 1,
-    'timestamp': 1506057125.900785,
-    'transactions': [
-        {
-            'sender': "8527147fe1f5426f9dd545de4b27ee00",
-            'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
-            'amount': 5,
-        }
-    ],
-    'proof': 324984774000,
-    'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
+	'index': 1,
+	'timestamp': 1506057125.900785,
+	'transactions': [
+		{
+			'sender': "8527147fe1f5426f9dd545de4b27ee00",
+			'recipient': "a77f5cdfa2934df3954a5c7c7da5df1f",
+			'amount': 5,
+		}
+	],
+	'proof': 324984774000,
+	'previous_hash': "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
 }
 """
 
@@ -30,8 +30,21 @@ class Blockchain(object):
 
 		self.current_transactions = []
 
-	def new_block(self):
-		pass
+		self.new_block(previous_hash=1, proof=100)
+
+	def new_block(self, proof, previous_hash=None):
+		
+		block = {
+			'index': len(self.chain)+1,
+			'timestamp': time(),
+			'transactions': self.current_transactions,
+			'proof': proof,
+			'previous_hash': previous_hash or self.hash(self.chain[-1])
+		}
+
+		self.current_transactions = []
+		self.chain.append(block)
+		return block
 
 	def new_transaction(self, sender, recipient, amount):
 
@@ -46,16 +59,77 @@ class Blockchain(object):
 		return self.last_block["index"]+1
 
 	def hash(self, block):
-		pass
+		block_string = json.dumps(block, sort_keys=True).encode()
+		return hashlib.sha256(block_string).hexdigest()
 
 	@property
 	def last_block(self):
-		return 0
+		return self.chain[-1]
+
+	def proof_of_work(self, last_proof):
+
+		proof = 0
+		while self.valid_proof(last_proof, proof) is False:
+			proof += 1
+
+		return proof
+
+	def valid_proof(self, last_proof, proof):
+
+		guess = f'{last_proof}{proof}'.encode()
+		guess_hash = hashlib.sha256(guess).hexdigest()
+		return guess_hash[:4] == "0000"
+
+
+	def valid_chain(self, chain):
+		
+        last_block = chain[0]
+        current_index = 1
+
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+
+            if block['previous_hash'] != self.hash(last_block):
+                return False
+
+            if not self.valid_proof(last_block['proof'], block['proof']):
+                return False
+
+            last_block = block
+            current_index += 1
+
+        return True
+
+    def resolve_conflicts(self):
+
+        neighbours = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        for node in neighbours:
+            response = requests.get(f'http://{node}/chain')
+
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if length > max_length and self.valid_chain(chain):
+                    max_length = length
+                    new_chain = chain
+
+        if new_chain:
+            self.chain = new_chain
+            return True
+
+        return False
 
 
 
 def main():
-	
 	GregCoin = Blockchain()
 	print(GregCoin.last_block)
 
---
+++ blockchain/test.py
@@ -0,0 +1,17 @@
+# libraries
+import mpl_toolkits
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+import matplotlib.pyplot as plt
+ 
+# A basic map
+m=Basemap()
+m.drawmapboundary(fill_color='black', linewidth=0)
+m.fillcontinents(color='black', alpha=0.7, lake_color='black')
+m.drawcoastlines(linewidth=0.1, color="white")
+ 
+# Add a connection between new york and London
+startlat = 40.78; startlon = -73.98
+arrlat = 51.53; arrlon = 0.08
+m.drawgreatcircle(startlon,startlat,arrlon,arrlat, linewidth=2, color='orange')
+plt.show()
