64a89f287e32af44d4e6ba99e742c129bd642020
Gregory H Eales <gregoeales@aol.com>
2021-11-28 11:56:28 -0800

    Fix indenting

--- blockchain/blockchain.py
+++ blockchain/blockchain.py
@@ -83,49 +83,49 @@ class Blockchain(object):
 
 	def valid_chain(self, chain):
 		
-        last_block = chain[0]
-        current_index = 1
+		last_block = chain[0]
+		current_index = 1
 
-        while current_index < len(chain):
-            block = chain[current_index]
-            print(f'{last_block}')
-            print(f'{block}')
-            print("\n-----------\n")
+		while current_index < len(chain):
+			block = chain[current_index]
+			print(f'{last_block}')
+			print(f'{block}')
+			print("\n-----------\n")
 
-            if block['previous_hash'] != self.hash(last_block):
-                return False
+			if block['previous_hash'] != self.hash(last_block):
+				return False
 
-            if not self.valid_proof(last_block['proof'], block['proof']):
-                return False
+			if not self.valid_proof(last_block['proof'], block['proof']):
+				return False
 
-            last_block = block
-            current_index += 1
+			last_block = block
+			current_index += 1
 
-        return True
+		return True
 
-    def resolve_conflicts(self):
+	def resolve_conflicts(self):
 
-        neighbours = self.nodes
-        new_chain = None
+		neighbours = self.nodes
+		new_chain = None
 
-        max_length = len(self.chain)
+		max_length = len(self.chain)
 
-        for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+		for node in neighbours:
+			response = requests.get(f'http://{node}/chain')
 
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
+			if response.status_code == 200:
+				length = response.json()['length']
+				chain = response.json()['chain']
 
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
+				if length > max_length and self.valid_chain(chain):
+					max_length = length
+					new_chain = chain
 
-        if new_chain:
-            self.chain = new_chain
-            return True
+		if new_chain:
+			self.chain = new_chain
+			return True
 
-        return False
+		return False
 
 
 
